7642a58d85f5bc0b4a1dc5fc21383cf4fe8c1963
Girish <girishsutrave1234@gmail.com>
2026-02-17 23:38:53 +0530

    Added the Home Controller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var employees = new List<string>
+            {
+                "John Doe",
+                "Jane Smith",
+                "Bob Johnson"
+            };
+            return Ok(employees);
+        }
+    }
+}
